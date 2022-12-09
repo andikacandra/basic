b653f1ae1dede137e3f4153fd33d2b064078ddd6
andikacandra <boynfs@gmail.com>
2022-12-09 21:18:32 +0700

    update

--- lib/section6/materi_6.dart
+++ lib/section6/materi_6.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -17,7 +18,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Dropdown Search')),
+      appBar: AppBar(title: const Text('Dropdown Search')),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: DropdownSearch(
+          items: ["Andika", "Candra", "Perdana", "Linda", "Desintasari"],
+          onChanged: (value) {
+            print(value);
+          },
+          selectedItem: "Candra",
+        ),
+      ),
     );
   }
 }
